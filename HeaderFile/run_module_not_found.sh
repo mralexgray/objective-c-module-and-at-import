1e38c0ffeccc943168d01622ea9de6bee9bc469d
Longyi Qi <longyi@chaione.com>
2012-03-07 17:04:08 -0600

    Use -ast-print flag to get AST and understand it clearly.

--- HeaderFile/run_module_not_found.sh
+++ HeaderFile/run_module_not_found.sh
@@ -5,5 +5,5 @@ rm -rf cache
 ../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_a module.map
 ../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_b module.map
 
-../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -fmodule-cache-path cache test_2.m -verify
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -fmodule-cache-path cache test_2.m -ast-print
 
--- HeaderFile/run_no_module_declared.sh
+++ HeaderFile/run_no_module_declared.sh
@@ -6,5 +6,5 @@ rm -rf cache
 ../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_b module.map
 ../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_c module.map
 
-../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -fmodule-cache-path cache test_1.m -verify
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -fmodule-cache-path cache test_1.m -ast-print
 
