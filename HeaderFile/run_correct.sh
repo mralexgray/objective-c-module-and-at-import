7c2cad7fe5c7b92b7187ec0767625309879ceaad
Longyi Qi <longyi@chaione.com>
2012-03-07 16:34:24 -0600

    initial commit with module on header files

---
+++ HeaderFile/run_correct.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+rm -rf cache
+
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_a module.map
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_b module.map
+
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -fmodule-cache-path cache test_1.m -verify
+
---
+++ HeaderFile/run_no_module_declared.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+rm -rf cache
+
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_a module.map
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_b module.map
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -emit-module -fmodule-cache-path cache -fmodule-name=module_c module.map
+
+../build/Debug+Asserts/bin/clang -cc1 -fmodules -x objective-c -fmodule-cache-path cache test_1.m -verify
+
